21edd19ca32c6a713108408ce6a3fff4ea83d64c
prajwal-sketch <shindeprajwal237@gmail.com>
2026-07-23 12:18:04 +0530

    monodb setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prajwal Shinde",
+    author_email="shindeprajwal237@gmail.com",
+    packages=find_packages()
+)
+
+
+
